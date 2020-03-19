669327b0959041404d04b87a586c56acc2e79d8e
trimbakeshwar <hudge.trimbakeshwar@gmail.com>
2020-03-19 13:44:55 +0530

    Added:-first arithmatic operation

--- arithmatic_operations.sh
+++ arithmatic_operations.sh
@@ -4,3 +4,9 @@ read -p "enter number: " a
 read -p "enter number: " b
 read -p "enter number: " c
 
+function arithmatic_computation_one(){
+   echo "$(( $a+$b*$c ))"
+}
+result="$( arithmatic_computation_one $(($a,$b,$c)) )"
+
+
